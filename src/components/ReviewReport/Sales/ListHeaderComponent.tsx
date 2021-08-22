067f9bf2a9f24112cb1c55423b0209dda74fc5ed
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-08-22 11:35:25 +0800

    Update report header for sales

--- src/components/ReviewReport/Sales/ListHeaderComponent.tsx
+++ src/components/ReviewReport/Sales/ListHeaderComponent.tsx
@@ -13,20 +13,13 @@ const ListHeaderComponent: FC = () => {
       <HorizontalLabel title="Custodian Code" subtitle="2av12e" />
       <HorizontalLabel title="Name" subtitle="Johnny Cash" />
       <HorizontalLabel title="Date Reported" subtitle={currentDate} />
-      <HorizontalLabel
-        title="Assignment"
-        subtitle="Mindanao Sales & Marketing"
-      />
 
-      <View style={styles.listSubHeader}>
-        <HorizontalLabel title="Revolving Fund Amount" subtitle="P8,000.00" />
-        <HorizontalLabel title="Replenishable Amount" subtitle="P6,445.00" />
-        <HorizontalLabel title="Unused Amount" subtitle="P1,594.00" />
+      <View style={styles.reportCycleLabelContainer}>
+        <HorizontalLabel bold title="Report Cycle Month" subtitle="JULY" />
+        <HorizontalLabel bold title="Report Year" subtitle="2021" />
       </View>
 
-      <HorizontalLabel title="Month of" subtitle="July 1, 2021" />
-
-      <Text style={styles.listHeaderTitle}>Summary Expense</Text>
+      <HorizontalLabel title="Invoice No" subtitle="503" />
     </View>
   )
 }
@@ -47,6 +40,9 @@ const styles = EStyleSheet.create({
     fontWeight: 'bold',
     alignSelf: 'center',
     marginTop: '$spacingMd',
+  },
+  reportCycleLabelContainer: {
+    paddingVertical: '$spacingSm',
   },
 })
 
