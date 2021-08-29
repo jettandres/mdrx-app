92d7df4cba677fa4477b2792ef732c56ea9ac4e9
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-08-29 16:11:49 +0800

    Layout basic custom drawer content

--- src/routes/HomeRoute.tsx
+++ src/routes/HomeRoute.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { View, Image } from 'react-native'
 
-import { createDrawerNavigator } from '@react-navigation/drawer'
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItem,
+  DrawerContentComponentProps,
+} from '@react-navigation/drawer'
 
 import type { FC } from 'react'
 
@@ -10,30 +16,78 @@ import SalesAndIncomeReport from '@screens/SummaryReports/SalesAndIncomeReport'
 import CollectionSummaryReport from '@screens/SummaryReports/CollectionSummaryReport'
 
 import { HomeDrawerParamList } from './types'
+import { Text } from 'react-native'
+import EStyleSheet from 'react-native-extended-stylesheet'
 
 const Drawer = createDrawerNavigator<HomeDrawerParamList>()
+
+import mdrxLogo from '@images/mdrx-logo.png'
 
 const HomeRoute: FC = () => {
   return (
     <>
       <Drawer.Navigator
         initialRouteName="Home"
-        screenOptions={{ drawerType: 'front' }}>
+        screenOptions={{ drawerType: 'front' }}
+        drawerContent={(props) => <CustomDrawerContent {...props} />}>
         <Drawer.Screen name="Home" component={Home} />
-        <Drawer.Group>
-          <Drawer.Screen name="ExpenseReport" component={ExpenseReport} />
-          <Drawer.Screen
-            name="SalesAndIncomeReport"
-            component={SalesAndIncomeReport}
-          />
-          <Drawer.Screen
-            name="CollectionSummaryReport"
-            component={CollectionSummaryReport}
-          />
-        </Drawer.Group>
+        <Drawer.Screen name="ExpenseReport" component={ExpenseReport} />
+        <Drawer.Screen
+          name="SalesAndIncomeReport"
+          component={SalesAndIncomeReport}
+        />
+        <Drawer.Screen
+          name="CollectionSummaryReport"
+          component={CollectionSummaryReport}
+        />
       </Drawer.Navigator>
     </>
   )
 }
+
+const CustomDrawerContent = (props: DrawerContentComponentProps) => {
+  const { navigation } = props
+  return (
+    <DrawerContentScrollView {...props}>
+      <Image source={mdrxLogo} style={styles.logo} />
+      <DrawerItem label="Home" onPress={() => navigation.navigate('Home')} />
+      <Text style={styles.drawerSectionHeader}>SUMMARY REPORTS</Text>
+      <View style={styles.drawerSectionContainer}>
+        <DrawerItem
+          label="Expense"
+          onPress={() => navigation.navigate('ExpenseReport')}
+        />
+        <DrawerItem
+          label="Sales and Income"
+          onPress={() => navigation.navigate('SalesAndIncomeReport')}
+        />
+        <DrawerItem
+          label="Collection"
+          onPress={() => navigation.navigate('CollectionSummaryReport')}
+        />
+      </View>
+      <DrawerItem label="Logout" onPress={() => navigation.goBack()} />
+    </DrawerContentScrollView>
+  )
+}
+
+const styles = EStyleSheet.create({
+  logo: {
+    height: 50,
+    width: '100%',
+    resizeMode: 'contain',
+    marginVertical: '$spacingMd',
+  },
+  drawerSectionHeader: {
+    fontSize: '$xs',
+    marginTop: '$spacingXs',
+    paddingHorizontal: '$spacingSm',
+    fontWeight: 'bold',
+    color: '$darkGray',
+  },
+  drawerSectionContainer: {
+    paddingHorizontal: '$spacingXs',
+  },
+})
 
 export default HomeRoute
