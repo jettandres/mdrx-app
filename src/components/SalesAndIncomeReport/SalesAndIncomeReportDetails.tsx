ae35fb8736386d2392338da678eeb8d08f46e852
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-08-30 12:50:59 +0800

    Add basic sectionlist

--- src/components/SalesAndIncomeReport/SalesAndIncomeReportDetails.tsx
+++ src/components/SalesAndIncomeReport/SalesAndIncomeReportDetails.tsx
@@ -1,17 +1,48 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, SectionList } from 'react-native'
 import EStyleSheet from 'react-native-extended-stylesheet'
 
 import type { FC } from 'react'
+
 import { useRoute } from '@react-navigation/core'
+import HorizontalLabel from '@components/HorizontalLabel'
+
+type SectionData = {
+  sales: string
+  income: string
+}
+
+type Sections = {
+  title: string
+  data: Array<SectionData>
+}
+
+const DATA: Array<Sections> = [
+  {
+    title: 'Jan 2021',
+    data: [{ sales: 'P100,000.00', income: 'P20,000.00' }],
+  },
+]
 
 const SalesAndIncomeReportDetails: FC = () => {
   const route = useRoute()
 
   return (
-    <View style={styles.container}>
-      <Text>{route.name}</Text>
-    </View>
+    <SectionList
+      keyExtractor={(_, index) => index.toString()}
+      sections={DATA}
+      renderSectionHeader={({ section: { title } }) => (
+        <View>
+          <Text>{title}</Text>
+        </View>
+      )}
+      renderItem={({ item: { sales, income }, section }) => (
+        <View>
+          <HorizontalLabel title="Sales" subtitle={sales} />
+          <HorizontalLabel title="Income" subtitle={income} />
+        </View>
+      )}
+    />
   )
 }
 
