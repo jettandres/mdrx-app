3536d6679fb4f21ce9c46d1f6dd7fa5867ce4960
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-09-05 15:56:30 +0800

    Style sales collection form page

--- src/screens/SalesCollectionForm.tsx
+++ src/screens/SalesCollectionForm.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button, Image, TouchableOpacity } from 'react-native'
 
 import type { FC } from 'react'
 import type { RootStackParamList } from '@routes/types'
@@ -8,6 +8,9 @@ import EStyleSheet from 'react-native-extended-stylesheet'
 
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import { RouteProp } from '@react-navigation/native'
+import HorizontalInput from '@components/HorizontalInput'
+
+import uploadIcon from '@images/outline_add_a_photo_black_24dp.png'
 
 type SalesCollectionNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -25,9 +28,22 @@ type Props = {
 
 const SalesCollectionForm: FC<Props> = (props) => {
   const { route } = props
+  const isSalesInvoice = route.params.collectionType === 'sales-invoice'
   return (
     <View style={styles.container}>
-      <Text>Sales Collection Form - {route.params.collectionType}</Text>
+      {isSalesInvoice && (
+        <HorizontalInput
+          title="Collection Receipt #"
+          placeholder="2312-2456-6431"
+        />
+      )}
+      <TouchableOpacity style={styles.uploadIconButton}>
+        <Image source={uploadIcon} style={styles.uploadIcon} />
+        <Text>Upload Deposit Slip</Text>
+      </TouchableOpacity>
+      <View style={styles.submitButton}>
+        <Button title="Submit" onPress={() => {}} />
+      </View>
     </View>
   )
 }
@@ -35,6 +51,22 @@ const SalesCollectionForm: FC<Props> = (props) => {
 const styles = EStyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'flex-start',
+    padding: '$spacingSm',
+    backgroundColor: '$white',
+  },
+  uploadIcon: {
+    height: 30,
+    width: 30,
+    marginBottom: '$spacingXs',
+  },
+  uploadIconButton: {
+    marginVertical: '$spacingMd',
+    alignSelf: 'center',
+    alignItems: 'center',
+  },
+  submitButton: {
+    width: '100%',
   },
 })
 
