612d9c1882534f5616027a5743e4efb2feed30e0
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-09-12 12:16:53 +0800

    Implement get employee query

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,12 +1,28 @@
 import React, { useCallback, useState, useEffect } from 'react'
-import { View, Text, TextInput, Button } from 'react-native'
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  ActivityIndicator,
+  TouchableOpacity,
+} from 'react-native'
 import EStyleSheet from 'react-native-extended-stylesheet'
 
 import type { FC } from 'react'
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import type { RouteProp } from '@react-navigation/native'
+import type Employee from '@app/types/Employee'
+
 import { RootStackParamList } from '@routes/types'
 import HorizontalLabel from '@components/HorizontalLabel'
+
+import { useLazyQuery } from '@apollo/client'
+import {
+  GET_EMPLOYEES,
+  GetEmployeesPayload,
+  GetEmployeeResponse,
+} from '@app/apollo/gql/employees'
 
 type LoginNavigationProp = NativeStackNavigationProp<
   RootStackParamList,
@@ -26,6 +42,9 @@ enum WizardStep {
 
 const Login: FC<Props> = (props) => {
   const { navigation } = props
+  const [custodianCode, setCustodianCode] = useState<string | undefined>()
+  const [errorLabel, setErrorLabel] = useState<string | undefined>()
+  const [employee, setEmployee] = useState<Employee | undefined>()
 
   const [currentStep, setCurrentStep] = useState<WizardStep>(
     WizardStep.InputCustodianCode,
@@ -34,12 +53,17 @@ const Login: FC<Props> = (props) => {
   const [nextButtonCopy, setNextButtonCopy] = useState('Search')
   const [titleCopy, setTitleCopy] = useState('Custodian Code')
 
+  const [getEmployee, { loading, error, data }] = useLazyQuery<
+    GetEmployeeResponse,
+    GetEmployeesPayload
+  >(GET_EMPLOYEES, { fetchPolicy: 'no-cache' })
+
   useEffect(() => {
     if (currentStep === WizardStep.InputCustodianCode) {
-      setNextButtonCopy('Search')
+      setNextButtonCopy('SEARCH')
       setTitleCopy('Custodian Code')
     } else if (currentStep === WizardStep.ConfirmCustodianCode) {
-      setNextButtonCopy('Confirm')
+      setNextButtonCopy('CONFIRM')
       setTitleCopy('Employee Info')
     }
   }, [currentStep])
@@ -48,33 +72,70 @@ const Login: FC<Props> = (props) => {
     if (currentStep === WizardStep.ConfirmCustodianCode) {
       navigation.navigate('HomeDrawer')
     } else if (currentStep === WizardStep.InputCustodianCode) {
-      // TODO: retrieve from database
-      setCurrentStep(WizardStep.ConfirmCustodianCode)
+      if (custodianCode === undefined) {
+        setErrorLabel('Field is required')
+      } else {
+        setErrorLabel(undefined)
+        console.log(custodianCode)
+        getEmployee({ variables: { code: custodianCode } })
+      }
     }
-  }, [navigation, currentStep])
+  }, [navigation, currentStep, custodianCode, getEmployee])
 
-  const onPrevButtonPress = useCallback(
-    () => setCurrentStep(WizardStep.InputCustodianCode),
-    [],
-  )
+  useEffect(() => {
+    if (data && data.employees.length) {
+      const [employeeData] = data.employees
+      setEmployee(employeeData)
+      setCurrentStep(WizardStep.ConfirmCustodianCode)
+    } else if (data && !data.employees.length) {
+      setErrorLabel('Employee not found')
+    }
+  }, [data])
+
+  useEffect(() => {
+    if (error) {
+      setErrorLabel(error.message)
+    }
+  }, [error])
+
+  const onPrevButtonPress = useCallback(() => {
+    setEmployee(undefined)
+    setCustodianCode(undefined)
+    setCurrentStep(WizardStep.InputCustodianCode)
+  }, [])
+
+  const onChangeCustodianInput = useCallback((value: string | undefined) => {
+    setCustodianCode(value)
+  }, [])
 
   return (
     <View style={styles.container}>
       <Text>{titleCopy}</Text>
       {currentStep === WizardStep.InputCustodianCode && (
-        <TextInput style={styles.textInput} placeholder="Custodian Code" />
+        <View style={styles.textInputContaienr}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Custodian Code"
+            value={custodianCode}
+            onChangeText={onChangeCustodianInput}
+          />
+          {!!errorLabel && <Text style={styles.errorLabel}>{errorLabel}</Text>}
+        </View>
       )}
-      {currentStep === WizardStep.ConfirmCustodianCode && (
+      {currentStep === WizardStep.ConfirmCustodianCode && employee && (
         <View style={styles.userInfoContainer}>
-          <HorizontalLabel title="Name" subtitle="Johnny Cash " />
+          <HorizontalLabel title="Name" subtitle={employee.name} />
           <HorizontalLabel
             title="Assignment"
-            subtitle="Mindanao Sales & Marketing"
+            subtitle={employee.custodianAssignment}
           />
-          <HorizontalLabel title="Area" subtitle="Manila" />
-          <HorizontalLabel title="Contact No." subtitle="0906123456" />
+          <HorizontalLabel title="Area" subtitle={employee.area} />
+          <HorizontalLabel
+            title="Contact No."
+            subtitle={employee.contactNumber}
+          />
 
-          <HorizontalLabel title="Email" subtitle="johnny@mdrx.net" />
+          <HorizontalLabel title="Email" subtitle={employee.email} />
         </View>
       )}
       <View style={styles.buttonsContainer}>
@@ -83,7 +144,16 @@ const Login: FC<Props> = (props) => {
             <Button title="Cancel" onPress={onPrevButtonPress} />
           </View>
         )}
-        <Button title={nextButtonCopy} onPress={onNextButtonPress} />
+        <TouchableOpacity style={styles.nextButton} onPress={onNextButtonPress}>
+          {loading && (
+            <ActivityIndicator
+              style={styles.loading}
+              animating
+              color="#007aff"
+            />
+          )}
+          <Text style={styles.nextButtonLabel}>{nextButtonCopy}</Text>
+        </TouchableOpacity>
       </View>
     </View>
   )
@@ -93,26 +163,46 @@ const styles = EStyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'flex-start',
     backgroundColor: '$white',
+    paddingHorizontal: '$spacingSm',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '$borderColor',
     borderRadius: 8,
+    paddingHorizontal: '$spacingSm',
+  },
+  textInputContaienr: {
     marginTop: '$spacingXs',
     marginBottom: '$spacingSm',
+    width: '100%',
   },
   buttonsContainer: {
     flexDirection: 'row',
+    justifyContent: 'center',
+    width: '100%',
+    alignItems: 'center',
   },
   cancelButton: {
     marginRight: '$spacingXs',
   },
   userInfoContainer: {
-    width: '80%',
+    width: '100%',
     marginBottom: '$spacingMd',
     marginTop: '$spacingSm',
+  },
+  errorLabel: {
+    color: '$red',
+  },
+  nextButton: {
+    flexDirection: 'row',
+  },
+  nextButtonLabel: {
+    color: '$blue',
+  },
+  loading: {
+    marginRight: '$spacingSm',
   },
 })
 
