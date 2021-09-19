8706c8975aef052ace82b336e5eac707a3cc62e9
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-09-19 08:42:46 +0800

    Implement react-hook-form with generic component

--- src/components/HorizontalPicker.tsx
+++ src/components/HorizontalPicker.tsx
@@ -5,6 +5,9 @@ import { Picker } from '@react-native-picker/picker'
 import EStyleSheet from 'react-native-extended-stylesheet'
 
 import type { FC } from 'react'
+import type { Control } from 'react-hook-form'
+
+import { useController } from 'react-hook-form'
 
 export type PickerItem = {
   label: string
@@ -13,20 +16,22 @@ export type PickerItem = {
 
 type Props = {
   title: string
-  onValueChange?: (value: unknown, index: number) => void
-  selectedValue?: unknown
   items: Array<PickerItem>
   borderless?: boolean
+  //eslint-disable-next-line @typescript-eslint/no-explicit-any
+  control: Control<any>
+  name: string
 }
 
 const HorizontalPicker: FC<Props> = (props) => {
-  const {
-    onValueChange,
-    selectedValue,
-    items,
-    title,
-    borderless = false,
-  } = props
+  const { items, title, borderless = false, control, name } = props
+
+  const { field } = useController({
+    control,
+    defaultValue: items[0].value,
+    name,
+  })
+
   return (
     <View style={styles.container}>
       <Text>{title}</Text>
@@ -34,7 +39,9 @@ const HorizontalPicker: FC<Props> = (props) => {
         style={
           borderless ? styles.pickerContainerBorderless : styles.pickerContainer
         }>
-        <Picker onValueChange={onValueChange} selectedValue={selectedValue}>
+        <Picker
+          onValueChange={(v: PickerItem) => field.onChange(v.value)}
+          selectedValue={items.find((i) => i.value === field.value)}>
           {items.map((i) => (
             <Picker.Item key={i.label} label={i.label} value={i.value} />
           ))}
