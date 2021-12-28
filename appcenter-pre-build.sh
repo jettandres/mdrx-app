307d310d9cfee1bf29c5299e7dc1090d340b9359
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-12-28 18:00:48 +0800

    Use a different gradle version

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 # Creates an .env from ENV variables for use with react-native-config
+(echo ""; echo "org.gradle.java.home=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/") >> $APPCENTER_SOURCE_DIRECTORY/gradle.properties
 ENV_WHITELIST=${ENV_WHITELIST:-"^RN_"}
+
 printf "Creating an .env file with the following whitelist:\n"
 printf "%s\n" $ENV_WHITELIST
 set | egrep -e $ENV_WHITELIST | sed 's/^RN_//g' > .env
