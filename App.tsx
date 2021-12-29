4549393219aa3394505bac66d37566ad84609066
Jett Robin Andres <jettrobin.andres@gmail.com>
2021-12-29 09:10:22 +0800

    Migrate aws backend config

--- App.tsx
+++ App.tsx
@@ -13,15 +13,15 @@ Amplify.configure({
   Auth: {
     mandatorySignIn: true,
     region: 'us-east-2',
-    userPoolId: 'us-east-2_Iy0BSewX6',
-    identityPoolId: 'us-east-2:75efa0c2-af07-412b-8dcb-b654ab7ec9ce',
-    userPoolWebClientId: '3319p5d89bdri6mhf5k6o3q1l4',
-    authenticationFlowType: 'USER_SRP_AUTH',
+    userPoolId: 'us-east-2_US3vdbubx',
+    identityPoolId: 'us-east-2:d4083231-4208-455e-805b-3ea69381f31c',
+    userPoolWebClientId: '6d7cn2cc1d8vv9jvq8h2npk137',
+    authenticationFlowType: 'USER_PASSWORD_AUTH',
   },
   Storage: {
     AWSS3: {
       region: 'us-east-2',
-      bucket: 'prod-mdrx-sst-storage-uploadsbucketc4b27cc7-1x13z9ge6o5pa',
+      bucket: 'prod-mdrx-sst-storage-uploadsbucketc4b27cc7-zqrj85rjsjxa',
     },
   },
   Analytics: {
